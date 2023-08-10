c4acc22fddd013295b188263ff1f4555246eeba8
Tristan Trinh <Tristan.Trinh8@gmail.com>
2023-08-10 09:29:07 -0400

    solo project

--- calendarApp/script.js
+++ calendarApp/script.js
@@ -8,6 +8,7 @@ const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 function getCalendar() {
     const dateAndTime = new Date();
     
+    //This get the current date
     const day = dateAndTime.getDate();
     const month = dateAndTime.getMonth();
     const year = dateAndTime.getFullYear();
@@ -16,9 +17,19 @@ function getCalendar() {
     //To get how many days in the month
     const daysInMonth = new Date(year, month+1, 0).getDate();
     console.log(daysInMonth);
-    const firstDayInMonth = new Date(year, month, 1);
 
-    
+    //This is to get the first day of the month to load, 'en-us' is just English calendar
+    const firstDayInMonth = new Date(year, month, 1);
+    const dateString = firstDayInMonth.toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+    });
+    console.log(dateString);
+    //This line will populate the first week of the month correctly, the "split(', ')" gets us the days from the previous month so our calender doesn't populate those dates from previous month
+    const daysInThisMonth = weekdays.indexOf(dateString.split(', ')[0]);
+    console.log(daysInThisMonth);
 }
 
 getCalendar();
